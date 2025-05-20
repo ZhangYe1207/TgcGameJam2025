200dab566cedaf771d33955284f42c1da87d1ff1
ZhangYe1207 <zhangye120707@gmail.com>
2025-05-20 17:11:52 +0800

    Init repo.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 15f;
+    public float maxSpeed = 30f;
+    public float decelerationForce = 10f; // Force applied when no input
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        
+        // Check if there's any input
+        if (Mathf.Abs(moveX) < 0.1f && Mathf.Abs(moveZ) < 0.1f)
+        {
+            // Apply deceleration force in the opposite direction of current velocity
+            if (rb.velocity.magnitude > 0.1f)
+            {
+                rb.AddForce(-rb.velocity.normalized * decelerationForce, ForceMode.Force);
+            }
+        }
+        else
+        {
+            Vector3 move = new Vector3(moveX, 0, moveZ) * speed;
+            rb.AddForce(move, ForceMode.Force);
+        }
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+}
