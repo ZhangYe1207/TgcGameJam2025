0a05fb7ef22f17f8f71b5d2f74ba7391b564cf0f
juface <systemerror101326@gmail.com>
2025-06-08 08:44:16 +0800

    相机快照，跟随玩家，灯光位置记录，关卡视角切换

---
+++ Assets/Scripts/CinemachineLevelData.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[System.Serializable]
+public class VirtualCameraSettings
+{
+    public Vector3 followOffset = new Vector3(0, 5, -10);
+    public float fov = 60f;
+    public Vector3 position = Vector3.zero;
+    public Vector3 rotation = Vector3.zero;
+}
+
+[System.Serializable]
+public class CinemachineLevelData
+{
+    [Header("Virtual Camera Settings")]
+    public VirtualCameraSettings overview = new VirtualCameraSettings();
+    public VirtualCameraSettings follow = new VirtualCameraSettings();
+
+    [Header("Main Light Settings")]
+    public Vector3 mainLightPosition = Vector3.zero;
+    public Vector3 mainLightRotation = Vector3.zero;
+
+    [Header("Air Wall Reference")]
+    public GameObject airWallRoot;
+}
